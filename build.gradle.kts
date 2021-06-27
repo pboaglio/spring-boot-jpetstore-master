2d386ae7a0c80eaba1a229fe8ad0bfa00fd9901f
pboaglio <85460061+pboaglio@users.noreply.github.com>
2021-06-27 18:17:55 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     id("java")
     id("com.diffplug.eclipse.apt") version "3.29.1"
-    id("com.diffplug.spotless") version "5.12.5"
+    id("com.diffplug.spotless") version "5.0.0"
     id("org.springframework.boot") version "2.5.0"
-    id("org.seasar.doma.compile") version "1.1.0"
+    id("org.seasar.doma.compile") version "1.0.0"
 }
 
 apply(plugin = "io.spring.dependency-management")
+
+version = "2.36.0-beta-1"
+val dependentVersion = version
+val domaSpringBootVersion = "1.4.0"
 
 spotless {
     java {
@@ -32,22 +36,44 @@ repositories {
 dependencies {
     val domaVersion: String by project
     val domaSpringBootVersion: String by project
-    annotationProcessor("org.seasar.doma:doma-processor:${domaVersion}")
+    annotationProcessor("org.seasar.doma:doma-processor:${dependentVersion}")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.3.0.RC1")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.seasar.doma:doma-core:${domaVersion}")
+    //implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.seasar.doma:doma-core:${dependentVersion}")
     implementation("org.seasar.doma.boot:doma-spring-boot-starter:${domaSpringBootVersion}")
+
     implementation("com.h2database:h2:1.4.200")
     implementation("org.webjars:jquery:3.6.0")
-    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.3")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
+    compileOnly("org.springframework.boot:spring-boot-devtools")
+
+    //implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.3")
+    //developmentOnly("org.springframework.boot:spring-boot-devtools")
+    //testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //{
+    //    exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    //}
+    
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.3.0.RC1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.3.3")
+
+    //Agregado esto para poder usar JUnit3-4 y probar con el framework de JUnit5
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testCompileOnly("junit:junit:4.13")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+
 }
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 
 eclipse {
     classpath {
@@ -72,8 +98,8 @@ eclipse {
     }
 }
 
-tasks {
-    test {
-        useJUnitPlatform()
-    }
-}
+//tasks {
+//    test {
+//        useJUnitPlatform()
+//    }
+//}
